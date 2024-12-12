4c09013ab73888e09e28d7aedac0fd6522e7d72b
Davide Gonnelli <davide.gonnelli1@gmail.com>
2024-12-12 17:38:40 +0100

    DOM structure

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 */
+// DOM elements
+const imgLampElm = document.getElementById("lamp");
+const buttonElm = document.getElementById("btn");
+
+// DOM events
+buttonElm.addEventListener("click",function() {
+
+});
